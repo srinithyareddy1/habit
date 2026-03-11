155316d709d5abf5e723fa19cef486908e1fee85
srinithyareddy1 <nithyareddgy569@gmail.com>
2026-03-11 20:26:24 +0530

    connect frontend to backend

--- habit-ui/src/services/api.js
+++ habit-ui/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API=axios.create({
-    baseURL: "http://localhost:8080"
+baseURL: "https://habit-o09t.onrender.com"
 });
 
 export const getHabits= () => API.get("/habits");
